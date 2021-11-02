190b80193fe2b02ad8f7e395448c7e77e0c48baf
Leandro Sena <leandrosena_02@hotmail.com>
2021-11-02 17:05:56 -0300

    reentrega

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row } from 'react-bootstrap';
+// import { Row } from 'react-bootstrap';
 
 function app() {
   return (
-    <NavBar class={Row}></NavBar>
+    <NavBar></NavBar>
   );
 }
 
--- proyecto-react/src/components/NavBar.js
+++ proyecto-react/src/components/NavBar.js
@@ -4,39 +4,38 @@ function NavBar() {
     return (
         <>
             <header>
-                <nav class=" barra navbar navbar-expand-lg navbar-dark ">
-                    <div class="container-fluid ">
+                <nav className=" barra navbar navbar-expand-lg navbar-dark ">
+                    <div className="container-fluid ">
 
-                        <a class="navbar-brand " href="#" ><h3>Los Sauces</h3><h5 class="" >Muebleria</h5> </a>
+                        <a className="navbar-brand " href="#" ><h3>Los Sauces</h3><h5 >Muebleria</h5> </a>
 
 
                         <div  style={{
                             position: 'absolute', left: '50%', top: '50%',
                             transform: 'translate(-50%, -50%)',
-                            colors:' #88aaaa'
                         }} >
-                            <ul class=" nav "  >
-                                <li class="nav-item ">
-                                    <a class="nav-link active text-secondary" aria-current="page" href="#">Inicio</a>
+                            <ul className=" nav "  >
+                                <li className="nav-item ">
+                                    <a className="nav-link active text-secondary " aria-current="page" href="#">Inicio</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-secondary" href="#">Productos</a>
+                                <li className="nav-item">
+                                    <a className="nav-link text-secondary" href="#">Productos</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link text-secondary" href="#">Home & Deco</a>
+                                <li className="nav-item">
+                                    <a className="nav-link text-secondary" href="#">Home & Deco</a>
                                 </li>
                             </ul>
                         </div>
 
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
 
-                        <ul class="navbar-nav">
-                            <li class="nav-item dropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item dropdown">
                             </li>
                         </ul>
-                        <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+                        <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
 
                         </div>
                     </div>
